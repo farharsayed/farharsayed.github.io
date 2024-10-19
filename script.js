39ef7103c803d636a3d423d95d3d995556221910
Farha Sayed <sc21frs@leeds.ac.uk>
2024-10-19 18:46:14 +0100

    current

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function toggleMenu() {
   const icon = document.querySelector(".hamburger-icon");
 
   // the action on those elements
+  // basically, when we click hamburger, it selects the open functions
+  // when we close hamburger, it selects the regular fns
   menu.classList.toggle("open")
   icon.classList.toggle("open")
 }
